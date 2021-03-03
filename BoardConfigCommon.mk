ef58b6352a17445ddb88d27fe7893b2eeb55caf8
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-03-03 13:18:16 +0100

    sm8350-common: Kill console parameters
    Change-Id: I69a14e00cfc3291175351259f2d8cbd6b0dc827a
    
    Signed-off-by: klozz <carlosj@klozz.dev>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -91,12 +91,10 @@ TARGET_KERNEL_NO_GCC := true
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm8350
 TARGET_KERNEL_CONFIG := vendor/lahaina-qgki_defconfig vendor/debugfs.config vendor/xiaomi_QGKI.config
 BOARD_RAMDISK_USE_LZ4 := true
-BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom
 BOARD_KERNEL_CMDLINE += androidboot.memcg=1
 BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_CMDLINE += cgroup.memory=nokmem,nosocket
-BOARD_KERNEL_CMDLINE += console=ttyMSM0,115200n8
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
 BOARD_KERNEL_CMDLINE += service_locator.enable=1

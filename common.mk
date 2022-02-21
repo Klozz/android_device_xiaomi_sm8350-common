cbb33e9dbb3756b2141223c6173eccfaa2ea0258
Martin Liu <liumartin@google.com>
2022-02-21 16:33:28 +0800

    sm8350-common: Set lmk stall_limit_critical threshold
    To prevent APC watchdog under heavy memory pressure,
    set stall_limit_critical to 40.
    
    Bug: 205182133
    Test: Run ACT under a heavy memory pressure
    Signed-off-by: Martin Liu <liumartin@google.com>
    Change-Id: Iaac006d1b1f2fff8a47875e792f6d2179bae80a9
    Merged-In: Iaac006d1b1f2fff8a47875e792f6d2179bae80a9
    (cherry picked from commit 360afd734baac29571b771424013edb6695e8619)
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: klozz <carlosj@klozz.dev>

--- common.mk
+++ common.mk
@@ -361,7 +361,8 @@ PRODUCT_PACKAGES += \
 # LMK
 PRODUCT_SYSTEM_PROPERTIES += \
     ro.lmk.filecache_min_kb=153600  \
-    ro.lmk.kill_timeout_ms=50
+    ro.lmk.kill_timeout_ms=50 \
+    ro.lmk.stall_limit_critical=40
 
 # Logging
 SPAMMY_LOG_TAGS := \

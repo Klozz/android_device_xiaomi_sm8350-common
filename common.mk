7867030492abb57a686d319d33572ea6362a1e66
Jprimero15 <jprimero15@aospa.co>
2023-12-24 09:25:24 +0800

    sm8350-common: Import Custom QTI AIDL Health HAL
    * From: LA.VENDOR.13.2.0.r1-21700-KAILUA.QSSI14.0
    * HEAD: https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/healthd-ext/-/commit/18028fe2c6bb3ef65ccb90639e15b74132a28a50
    * Reverted commit: "healthd-ext: add suspend support for charger mode"
    * Overrides normal QTI AIDL health hal.
    * Build it, added branding and sepolicy.
    
    Change-Id: Ia95149ac057f1449d26fb30dbc47b0f991678f3b
    
    Signed-off-by: klozz <carlosj@klozz.dev>

--- common.mk
+++ common.mk
@@ -296,8 +296,8 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1.vendor \
-    android.hardware.health-service.qti
+    android.hardware.health-service.qti.lahaina \
+    android.hardware.health@2.1.vendor
 
 PRODUCT_PACKAGES += \
     libhidltransport.vendor \

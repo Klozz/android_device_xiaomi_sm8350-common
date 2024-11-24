4aed2353852146573aad111a430c7e01b756a663
Jake Weinstein <jake@aospa.co>
2024-11-24 21:16:48 +0530

    sm8350-common: Dynamically set IRQ affinity for KGSL and  MSM_DRM
    - Introduce a new shell script to set IRQ affinity, since different
      kernels can have different IRQ numbers.
    
    Co-Authored-By: Alex Naidis <alex.naidis@paranoidandroid.co>
    Co-Authored-By: Jake Weinstein <jake@aospa.co>
    Change-Id: I61d2350e6b40f096714148c568489579bc4d2371
    Signed-off-by: klozz <carlosj@klozz.dev>

--- extract-files.sh
+++ extract-files.sh
@@ -103,6 +103,9 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             echo 'gettid: 1' >> ${2}
             ;;
+        vendor/etc/msm_irqbalance.conf)
+            sed -i "s/IGNORED_IRQ=27,23,38$/&,115,332/" "${2}"
+            ;;
     esac
 }
 

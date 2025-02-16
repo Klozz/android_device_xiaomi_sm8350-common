dbed050f9881f8a4d19a74ff2196b8eaa216e63e
klozz <carlosj@klozz.dev>
2025-02-16 03:16:14 -0600

    sm8350-common: track APTX non-opensource
    Signed-off-by: klozz <carlosj@klozz.dev>

--- common.mk
+++ common.mk
@@ -120,6 +120,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
 
+# Get non-open-source specific aspects.
+$(call inherit-product-if-exists, vendor/qcom/common/system/bt/bt-vendor.mk)
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080

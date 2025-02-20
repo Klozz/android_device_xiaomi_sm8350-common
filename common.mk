9b5fb236094cda2a6deaa1873bddf776374350fb
klozz <carlosj@klozz.dev>
2025-02-19 20:15:02 -0600

    sm8350-common: Build android.hardware.audio.sounddose-vendor-impl
    this fix E audiohalservice: Failed to dlopen android.hardware.audio.sounddose-vendor-impl.so: dlopen failed: library "android.hardware.audio.sounddose-vendor-impl.so" not found
    
    Signed-off-by: klozz <carlosj@klozz.dev>

--- common.mk
+++ common.mk
@@ -54,6 +54,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
+    android.hardware.audio.sounddose-vendor-impl \
     android.hardware.soundtrigger@2.2-impl
 
 PRODUCT_COPY_FILES += \

a5edb06bbfe07c33820926293c73f709d5b75061
klozz <carlosj@klozz.dev>
2025-03-20 06:23:44 +0000

    sm8350-commondrop libdmabufheap
    Signed-off-by: klozz <carlosj@klozz.dev>

--- extract-files.py
+++ extract-files.py
@@ -45,7 +45,7 @@ lib_fixups: lib_fixups_user_type = {
         'vendor.qti.hardware.iop@2.0',
         'vendor.qti.qspmhal@1.0',
         'vendor.xiaomi.hardware.displayfeature@1.0',
-        'libstagefright_flacdec'
+        'libstagefright_flacdec',
     ): lib_fixup_vendor_suffix,
     (
         'libOmxCore',

47ec9a4fbf440719e7bf45abf8f0bc8dfb818395
klozz <carlosj@klozz.dev>
2025-04-30 04:24:40 +0000

    sm8350-common: adapt to VOS
    Signed-off-by: klozz <carlosj@klozz.dev>

--- common.mk
+++ common.mk
@@ -673,7 +673,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
 PRODUCT_PACKAGES += \
-    vendor.xperience.touch@1.0-service.xiaomi
+    vendor.lineage.touch@1.0-service.xiaomi
 
 # Ueventd
 PRODUCT_COPY_FILES += \

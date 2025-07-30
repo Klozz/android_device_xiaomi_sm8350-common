cdf806f28613fe40a8dba3641c01e76f4ff505db
kyasu <kyasu.dev@gmail.com>
2025-07-30 23:06:15 +0900

    sm8350-common: Disable `thumbnail_block_model` of libstagefright
    Change-Id: I86f92335af282d3de508150bbbcb3026993ec48c

--- common.mk
+++ common.mk
@@ -500,6 +500,8 @@ PRODUCT_PACKAGES += \
     libstagefright_softomx_plugin.vendor \
     libstagefright_softomx.vendor
 
+$(call soong_config_set,stagefright,target_disable_thumbnail_block_model,true)
+
 # Native libraries whitelist
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt

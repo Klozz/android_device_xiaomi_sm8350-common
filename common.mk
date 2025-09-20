e5b370ab2bf8fc221528f1b4235177c69433305d
klozz <carlosj@klozz.dev>
2025-09-20 05:05:20 -0600

    sm8350-common: Build sensorservice 1.0 HAL
    W HidlServiceManagement: Waited one second for android.frameworks.sensorservice@1.0::ISensorManager/default
    I hwservicemanager: Since android.frameworks.sensorservice@1.0::ISensorManager/default is not registered, trying to start it as a lazy HAL.
    I HidlServiceManagement: getService: Trying again for android.frameworks.sensorservice@1.0::ISensorManager/default...
    W libc    : Unable to set property "ctl.interface_start" to "android.frameworks.sensorservice@1.0::ISensorManager/default": error code: 0x20
    
    Signed-off-by: klozz <carlosj@klozz.dev>

--- common.mk
+++ common.mk
@@ -638,6 +638,7 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
+    android.frameworks.sensorservice@1.0 \
     android.hardware.sensors-service.xiaomi-multihal \
     vendor.qti.hardware.display.mapper@1.1.vendor
 

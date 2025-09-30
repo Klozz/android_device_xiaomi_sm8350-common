c1f24e030357048efbe8f1293dc2927737f0b32d
ianchb <i@4t.pw>
2025-09-30 15:40:54 +0800

    sm8350-common: Migrate Lineage power HAL to soong_config_set
    Change-Id: I7a449daa77e5e8e046d0c28be2ccae0a908e0ae9
    Signed-off-by: klozz <carlosj@klozz.dev>

--- common.mk
+++ common.mk
@@ -561,6 +561,8 @@ PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Power
+$(call soong_config_set,power_libperfmgr,mode_extension_lib,//$(LOCAL_PATH):power-mode-ext)
+
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2.vendor \
     android.hardware.power-service.lineage-libperfmgr
